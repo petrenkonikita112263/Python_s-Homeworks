d47249fe88fc2a630727b04cff3deb99bd87f439
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-22 17:17:26 +0300

    Created new folder for OOP exercises's solutions.
    Completed 1st exercise.

---
+++ OOP Review Exercises/run_classes.py
@@ -0,0 +1,6 @@
+from sphere import Sphere
+
+s = Sphere(1)
+s.surface_area()
+s.volume()
+print(s)
---
+++ OOP Review Exercises/sphere.py
@@ -0,0 +1,24 @@
+import math
+
+"""
+Exercise 1
+Create a Sphere class that accepts a radius upon instantiation and has a volume and surface area method.
+"""
+
+
+class Sphere:
+    # Create the methods here!
+    def __init__(self, radius: int) -> None:
+        self.radius = radius
+        self.area_value = 0.0
+        self.volume_value = 0.0
+
+    def surface_area(self) -> None:
+        self.area_value = 4 * math.pi * self.radius ** 2
+
+    def volume(self) -> None:
+        self.volume_value = (4 / 3) * math.pi * self.radius ** 2
+
+    def __str__(self) -> str:
+        return f"The sphere with radius = {self.radius} have surface area = {self.area_value}" \
+               f" and volume = {self.volume_value}"
