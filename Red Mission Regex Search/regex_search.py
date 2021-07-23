421be47d722e17ddc1699b7b82cff5df5cffd760
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-23 13:09:35 +0300

    Completed the task.
    Finds all the url links from the every txt files. Created a function, first creates the empty result file.
    After the url link was found through regex expressions, they are saved into founded_links.txt file.

--- Red Mission Regex Search/regex_search.py
+++ Red Mission Regex Search/regex_search.py
@@ -1,7 +1,15 @@
 import os
+import re
+
+
+def create_result_file():
+    with open("./founded_links.txt", "w") as result_file:
+        result_file.write("")
+
 
 root_dir = os.getcwd()
 # print(root_dir)
+create_result_file()
 
 for sub_dir, dirs, files in os.walk(root_dir):
     if sub_dir != root_dir:
@@ -11,4 +19,9 @@ for sub_dir, dirs, files in os.walk(root_dir):
                 # print(filepath)
                 with open(filepath) as f:
                     f_content = f.read()
-                    print(f_content)
+                    # print(f_content)
+                    urls = re.findall(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+                                      f_content)
+                    # print(urls)
+                    with open("./founded_links.txt", "a") as result_file:
+                        result_file.writelines(f"{urls}\n")
