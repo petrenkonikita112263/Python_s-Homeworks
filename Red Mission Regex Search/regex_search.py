7f0adf082be0f24cbc8edc8eeb4107c976cc38dd
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-23 12:43:40 +0300

    Browse all txt files in the root directory and subdirectories.

--- Red Mission Regex Search/regex_search.py
+++ Red Mission Regex Search/regex_search.py
@@ -1,4 +1,10 @@
 import os
 
 root_dir = os.getcwd()
-print(root_dir)
+# print(root_dir)
+
+for sub_dir, dirs, files in os.walk(root_dir):
+    for file in files:
+        filepath = sub_dir + os.sep + file
+        if filepath.endswith(".txt"):
+            print(filepath)
