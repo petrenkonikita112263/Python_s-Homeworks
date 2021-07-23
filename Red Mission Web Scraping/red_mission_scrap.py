f223fd35a21341b7b265178da979e4d2499f5e5c
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-23 11:59:07 +0300

    Get the text from blog in the page.

--- Red Mission Web Scraping/red_mission_scrap.py
+++ Red Mission Web Scraping/red_mission_scrap.py
@@ -5,4 +5,11 @@ URL_LINK = "https://www.thegoldbugs.com/blog"
 
 resp = req.get(URL_LINK)
 soup = bs4.BeautifulSoup(resp.text, "lxml")
-print(soup.prettify())
+# print(soup.prettify())
+
+blog_content = soup.select(".sqs-block-content > pre")
+# print(blog_content)
+text = blog_content[0]
+# print(text)
+text = text.contents[0]
+print(text)
