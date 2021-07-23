686674e205e3026013bef22150d83be562ed1263
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-23 12:53:55 +0300

    Excluded the instructions.file from wokring directory.

--- Red Mission Regex Search/regex_search.py
+++ Red Mission Regex Search/regex_search.py
@@ -4,7 +4,8 @@ root_dir = os.getcwd()
 # print(root_dir)
 
 for sub_dir, dirs, files in os.walk(root_dir):
-    for file in files:
-        filepath = sub_dir + os.sep + file
-        if filepath.endswith(".txt"):
-            print(filepath)
+    if sub_dir != root_dir:
+        for file in files:
+            filepath = sub_dir + os.sep + file
+            if filepath.endswith(".txt"):
+                print(filepath)
