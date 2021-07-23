8ccefe9f3d9fa555c0be8b65067d6122928c73ae
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-23 16:44:20 +0300

    Saved the url link into txt file. The result file download_link_to_pdf_file.txt.

--- Red Mission Email Address/find_in_csv.py
+++ Red Mission Email Address/find_in_csv.py
@@ -3,4 +3,7 @@ import pandas as pd
 df = pd.read_csv("download_link.csv", header=None)
 # print(df)
 # print(df[2])
-print("".join(df[2]))
+url_link = "".join(df[2])
+
+with open("download_link_to_pdf_file.txt", "w") as file:
+    file.write(url_link)
