9771e8095c6705eb116074417a3f51b4282b10b9
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-24 14:48:36 +0300

    Run Tesseract and read the text from image. Print all text without last line.

--- Red Mission Images/image_run.py
+++ Red Mission Images/image_run.py
@@ -7,3 +7,7 @@ decoder_image = Image.open("cover_image.PNG")
 image_link = Image.open("image_link.PNG")
 # decoder_image.show()
 # image_link.show()
+
+pytesseract.tesseract_cmd = TESSERACT
+text = pytesseract.image_to_string(image_link)
+print(text[:-1])
