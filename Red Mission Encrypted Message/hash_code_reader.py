63e5b272e27a6cc4d47b9e61a4ffd07c6e27df9d
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-24 15:35:33 +0300

    Got the diget of value of the highest prime number.

--- Red Mission Encrypted Message/hash_code_reader.py
+++ Red Mission Encrypted Message/hash_code_reader.py
@@ -3,5 +3,6 @@ import hashlib
 keyword = b'65537'
 encode_string = b'gAAAAABaSsmdCFRxbqA6n-L0CMIMuhn26uGiIk5Wtx-V7wEPLBZYA67nGbNWyZziGyorwIlHqp3M5xrtzQj5tCab8XfBRCmdJXZYD1Fwp68AtD8WEMhblQ4I2DKDNFzqULH1DDETry3ptZnGZUgVo5gdDlnihqu1_oX-KboNpyRQ6J0DmeWTsm3L31btF_O6sX81rj3rBVI0qVuT7QdRT2burisQRnw5htA05llYgc1_fMkN_PSxCwY='
 
-hash_object = hashlib.sha3_256(encode_string)
-print(hash_object.hexdigest())
+hash_object = hashlib.sha3_256(keyword)
+key = hash_object.digest()
+print(key)
