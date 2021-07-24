5a74184c50aa63a0f4a4cc88700c9ee9be8c3014
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-24 19:41:47 +0300

    Improved finding links from image.
    Resize cover_image to image_link. Added alpha to it. Put cover_image into image_link.

--- Red Mission Images/image_run.py
+++ Red Mission Images/image_run.py
@@ -5,8 +5,10 @@ TESSERACT = r"PATH TO EXE IN YOUR MACHINE"
 
 decoder_image = Image.open("cover_image.PNG")
 image_link = Image.open("image_link.PNG")
-# decoder_image.show()
-# image_link.show()
+decoder_image.putalpha(200)
+decoder_image = decoder_image.resize(image_link.size)
+image_link.paste(decoder_image, (0, 0), decoder_image)
+image_link.show()
 
 pytesseract.tesseract_cmd = TESSERACT
 text = pytesseract.image_to_string(image_link)
