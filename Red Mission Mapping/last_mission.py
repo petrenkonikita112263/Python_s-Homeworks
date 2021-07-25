4125e74cc857bb039d38cc340dee2728e03d3205
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-25 13:19:00 +0300

    Imported neccessary libraries for futher action. Created the coming plot object by GMapPlot class from GMaps. Obtained the api key.

--- Red Mission Mapping/last_mission.py
+++ Red Mission Mapping/last_mission.py
@@ -1,4 +1,10 @@
+from bokeh.io import output_file, show
+from bokeh.models import GMapPlot, GMapOptions, ColumnDataSource, \
+    Circle, Range1d, PanTool, WheelZoomTool, BoxSelectTool
+
 import re
+
+API_KEY = "AIzaSyAtt_C5_5WTwW-0bTHYi4aSDmURoU1UzvE"
 
 with open("message_new_pen.txt") as file:
     content = file.read()
@@ -14,3 +20,9 @@ for coordinate in coordinates:
     lat, long = coordinate.split(",")
     lats.append(lat)
     longs.append(long)
+
+map_options = GMapOptions(lat=0, lng=0, zoom=3)
+
+plot = GMapPlot(x_range=Range1d(), y_range=Range1d(), map_options=map_options)
+plot.title.text = "Example Plot"
+plot.api_key = API_KEY
